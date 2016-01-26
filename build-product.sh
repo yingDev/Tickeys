c78d26e838ad41d0e996e04c8fd0d7947df8d608
Ying <ying@YingdeMacBook-Pro.local>
2016-01-26 23:49:01 +0800

    update with rust 1.6; static lik openssl

--- build-product.sh
+++ build-product.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo "compiling...";
+
+export OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib
+export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
+export OPENSSL_STATIC=true
+
+cargo clean;
 cargo build --release;
 
 
